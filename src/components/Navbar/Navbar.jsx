7df25f279a8afd7b2f9ee1902233f32a95eec9f6
Ashu Negi <anegi6723@gmail.com>
2024-02-25 18:52:29 +0530

    added styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-700 text-white py-4 px-8 w-screen flex flex-col md:flex-row items-center'>
+    <nav className='bg-green-600 text-white py-4 px-8 w-screen flex flex-col md:flex-row items-center'>
         <div className='font-bold text-2xl'>
-            NAVBAR
+            GoFood
         </div>
         <div className='ml-8 '>
-            <ul className='flex flex-col md:flex-row text-white gap-3 list-none items-center'>
+            <ul className='flex flex-col md:flex-row  text-white gap-3 list-none items-center'>
                 <li>
                     <NavLink to="/">Home</NavLink>
                 </li>
@@ -19,7 +19,7 @@ const Navbar = () => {
                     <NavLink to="/">Contact</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/">Blog</NavLink>
+                    <NavLink to="/login">Login</NavLink>
                 </li>
             </ul>
         </div>
