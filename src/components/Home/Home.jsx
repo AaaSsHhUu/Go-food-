abc0ccc0ce68463c00efc4917dcaa99164127338
Ashu Negi <anegi6723@gmail.com>
2024-03-01 18:07:47 +0530

    added carousel to home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-import {Card} from '../index';
-
+import {Card, DefaultCarousel} from '../index';
+ 
 const Home = () => {
+
+  let slides = [
+     "https://source.unsplash.com/random/2100x600/?burger",
+     "https://source.unsplash.com/random/2100x600/?barbeque",
+     "https://source.unsplash.com/random/2100x600/?pizza",
+  ]
   return (
       <>
+        <div className=' from-gray-500 to-gray-500'>
+          <div className='w-full flex justify-center'>
+            <DefaultCarousel slides={slides} />
+          </div>
+        </div>
         <Card />
       </>
   )
