846bc70b1da8684092a9fab0e495bb0d6104baef
Ashu Negi <anegi6723@gmail.com>
2024-03-01 18:08:01 +0530

    created footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='w-full py-3 bg-gray-600 text-white text-center'>
+      <span className='italic'> &copy; 2024 GoFood Inc </span>
     </div>
   )
 }
