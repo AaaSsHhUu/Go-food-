48bc6bd744ccb78c8dc29e46f270bff7b12ac2d1
Ashu Negi <anegi6723@gmail.com>
2024-03-03 18:44:31 +0530

    created login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>
-      Login
+    <div className='bg-green-300 p-4 h-screen grid place-items-center'>
+        <div className='bg-white w-full sm:w-2/4 flex min-h-[50vh] py-4 rounded-lg flex-col items-center'>
+            <div className='w-[90%] sm:w-3/4 px-4 py-2 rounded-lg'>
+                <label htmlFor="username" className='my-2 font-bold'>Username</label>
+                <br />
+                <input type="text" id='username' placeholder='Enter Username' className='focus:outline-2 focus:outline-green-400 rounded-md my-2 border-2 border-none outline-none w-full bg-gray-200 px-4 py-2' required />
+            </div>
+
+            <div className='w-[90%] sm:w-3/4 px-4 py-2 rounded-lg'>
+                <label htmlFor="password" className='my-2 font-bold'>Password</label>
+                <br />
+                <input type="password" id='password' placeholder='Enter Password' className='focus:outline-2 focus:outline-green-400 rounded-md my-2 border-2 border-none outline-none w-full bg-gray-200 px-4 py-2' required />
+            </div>
+            <div className='text-center my-4'>
+                <button className='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-500'>Login</button>
+                <br />
+                <Link to={"/signup"} className='text-blue-600 hover:underline text-sm' >Don't have an account? create a new one</Link>
+            </div>
+        </div> 
     </div>
   )
 }
