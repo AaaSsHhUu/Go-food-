d9e954852e0dd89fe9d69f6d63290418217291b5
Ashu Negi <anegi6723@gmail.com>
2024-03-03 11:32:59 +0530

    created searchbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='bg-green-600 text-white py-4 px-8 w-full flex flex-col md:flex-row items-center'>
-        <div className='font-bold text-2xl'>
+    <nav className='bg-green-600 text-white py-4 px-12 w-full flex flex-col md:flex-row justify-evenly items-center'>
+        <div className='font-bold text-3xl'>
             GoFood
         </div>
-        <div className='ml-8 '>
-            <ul className='flex flex-col md:flex-row  text-white gap-3 list-none items-center'>
+        <div className='ml-8 w-full flex flex-col sm:flex-row justify-evenly items-center'>
+            <ul className='flex flex-col md:flex-row text-white gap-8 text-lg font-bold list-none items-center'>
                 <li>
                     <NavLink to="/">Home</NavLink>
                 </li>
@@ -22,6 +22,10 @@ const Navbar = () => {
                     <NavLink to="/login">Login</NavLink>
                 </li>
             </ul>
+        </div>
+        <div className='flex items-center'>
+            <input type="text" placeholder='What do you want to eat?' className='border-none w-60 outline-none rounded-l-md px-3 py-2 text-gray-700'/>
+            <button className='bg-green-400 text-white font-bold rounded-r-md py-2 px-3'>Search</button>
         </div>
     </nav>
   )
