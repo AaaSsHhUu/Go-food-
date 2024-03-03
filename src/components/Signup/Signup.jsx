e0fe773309fa5eebc2002c2cd8110cb57babfae3
Ashu Negi <anegi6723@gmail.com>
2024-03-03 12:10:28 +0530

    styled signup page

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,18 +3,26 @@ import React from 'react'
 const Signup = () => {
   return (
     <div className='bg-green-300 h-screen grid place-items-center'>
-        <div className='bg-white w-2/4 flex flex-col items-center'>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" id='username' placeholder='Enter Username' required />
+        <div className='bg-white w-2/4 flex min-h-[60vh] py-4 rounded-lg flex-col items-center'>
+            <div className='w-3/4 px-4 py-2 rounded-lg'>
+                <label htmlFor="username" className='my-2 font-bold'>Username</label>
+                <br />
+                <input type="text" id='username' placeholder='Enter Username' className='focus:outline-2 focus:outline-green-400 rounded-md my-2 border-2 border-none outline-none w-full bg-gray-200 px-4 py-2' required />
             </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id='email' placeholder='Enter Email' required />
+
+            <div className='w-3/4 px-4 py-2 rounded-lg'>
+                <label htmlFor="email" className='my-2 font-bold'>Email</label>
+                <br />
+                <input type="email" id='email' placeholder='Enter Email' className='focus:outline-2 focus:outline-green-400 rounded-md my-2 border-2 border-none outline-none w-full bg-gray-200 px-4 py-2' required />
             </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" id='password' placeholder='Enter Password' required />
+
+            <div className='w-3/4 px-4 py-2 rounded-lg'>
+                <label htmlFor="password" className='my-2 font-bold'>Password</label>
+                <br />
+                <input type="password" id='password' placeholder='Enter Password' className='focus:outline-2 focus:outline-green-400 rounded-md my-2 border-2 border-none outline-none w-full bg-gray-200 px-4 py-2' required />
+            </div>
+            <div className='text-center my-4'>
+                <button className='bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-500'>Sign up</button>
             </div>
         </div> 
     </div>
