5d7e1555d627ef263b1d1780bf31634b0aec6816
Ashu Negi <anegi6723@gmail.com>
2024-03-07 10:32:59 +0530

    updated route name

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -14,7 +14,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/user/new", {
+    const response = await fetch("http://localhost:5000/user/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
