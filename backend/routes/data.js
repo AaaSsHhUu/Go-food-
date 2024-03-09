c7d9a08b2201edff464cfe27cabe9bdb7f73cb67
Ashu Negi <anegi6723@gmail.com>
2024-03-09 20:15:38 +0530

    created foodData route

---
+++ backend/routes/data.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/foodData",(req,res) => {
+    try{
+        res.send([global.food_data,global.foodCategory]);
+    }
+    catch(err){
+        res.send(err.message)
+    }
+})
+
+module.exports = router
