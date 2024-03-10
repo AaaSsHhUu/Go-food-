7b23b692631a898a75a9de7d5cb2c536892da44f
Ashu Negi <anegi6723@gmail.com>
2024-03-10 19:43:41 +0530

    modified card styling and used props

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({name,img,desc,options}) {
+
+  let option = options;
+  let priceOptions = Object.keys(option)
+
   return (
     <>
-      <div className="bg-white rounded-md m-3 w-80 h-56 mt-3">
-        <div className="w-80 max-h-96 m-3 flex flex-col">
-          <img src="" className="" />
-          <div className="">
-            <h5 className="font-bold text-xl">Card Title</h5>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-              sed? Aliquid quod modi perferendis? Error.
+      {/* Outer div */}
+      <div className="bg-white rounded-md w-80 h-[24rem] mt-3 text-black overflow-hidden">
+        {/* Inner div */}
+        <div className="w-full h-full flex flex-col ">
+          <img src={img} className="w-full h-2/4" />
+            <h5 className="font-bold text-xl my-2">{name}</h5>
+            <p className="px-2 mb-2 text-gray-700">
+              {desc}
             </p>
-            <div className="w-full flex flex-row items-center gap-3">
+            <div className="w-full flex flex-row justify-evenly my-2 items-center gap-3">
               <select className="mx-2 bg-green-600 text-white outline-none rounded-md">
                 {Array.from(Array(6), (elem, index) => {
                   return (
@@ -23,12 +27,11 @@ export default function Card() {
                 })}
               </select>
               <select className="mx-2 bg-green-600 py-1 px-2 text-white outline-none rounded-md">
-                <option key={1} value="half">
-                  Half
-                </option>
-                <option key={2} value="full">
-                  Full
-                </option>
+                {
+                  priceOptions.map((data) => {
+                    return <option key={data} value={data}>{data}</option>
+                  })
+                }
               </select>
 
               <div className="bg-green-600 py-1 px-2 text-white rounded-lg">
@@ -36,7 +39,6 @@ export default function Card() {
               </div>
             </div>
           </div>
-        </div>
       </div>
     </>
   );
