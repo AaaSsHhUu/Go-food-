2463620f5c992bfba50ccdf1fd48e2e3b7fa316c
Ashu Negi <anegi6723@gmail.com>
2024-03-12 12:01:25 +0530

    wrapped components inside nav context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import NavContextProvider from './context/NavContextProvider'
 
 function App() {
 
   return (
-      <>
+      <NavContextProvider>
         <Navbar />
         <Outlet />
         <Footer />
-      </>   
+      </NavContextProvider>   
   )
 }
 
