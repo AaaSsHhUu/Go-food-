f338b05cec8ba4f4e633bc584b49fdb99d8f2972
Ashu Negi <anegi6723@gmail.com>
2024-03-13 11:46:21 +0530

    used async handler to handle async errors

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const {createUser, loginUser} = require("../controller/user");
 const { body } = require("express-validator");
+const asyncHandler = require("express-async-handler")
 
 
 router.post("/signup",[
     body("email","Invalid email").isEmail(),
     body("name","Invalid username").isLength({min : 2}),
     body("password","Invalid password").isLength({min : 5})
-], createUser)
+], asyncHandler(createUser))
 
-router.post("/login", loginUser)
+router.post("/login", asyncHandler(loginUser));
 
 module.exports = router;
