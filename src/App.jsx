760914342f44d95786b65570155d00ced35aa2d3
Ashu Negi <anegi6723@gmail.com>
2024-03-17 18:55:51 +0530

    wrapping outlet inside CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import NavContextProvider from './context/NavContextProvider'
+import { CartProvider } from './context/ContextReducer'; 
+
 
 function App() {
 
   return (
       <NavContextProvider>
         <Navbar />
-        <Outlet />
+        <CartProvider>
+          <Outlet />
+        </CartProvider>
         <Footer />
       </NavContextProvider>   
   )
