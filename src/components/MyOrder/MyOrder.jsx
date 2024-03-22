8d09972947c2999248a048e7eeb2129ec579d39b
Ashu Negi <anegi6723@gmail.com>
2024-03-22 13:20:36 +0530

    created myorder and added its route

---
+++ src/components/MyOrder/MyOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyOrder() {
+  return (
+    <div>
+      mt order
+    </div>
+  )
+}
+
+export default MyOrder
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom'
-import { Home, Login, Signup,Contact, Cart } from './components/index.js';
+import { Home, Login, Signup,Contact, MyOrder } from './components/index.js';
 
 
 
@@ -14,6 +14,7 @@ const router = createBrowserRouter(
       <Route path="contact" element={<Contact />} />
       <Route path='login' element={<Login />} />
       <Route path='signup' element={<Signup />} />
+      <Route path='myorder' element={<MyOrder />} />
     </Route>
   )
 )
