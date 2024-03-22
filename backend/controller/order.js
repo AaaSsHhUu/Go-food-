50deaabb906d2ea8069b50aee3871f48ec9867ec
Ashu Negi <anegi6723@gmail.com>
2024-03-22 12:26:34 +0530

    resolved order route bug

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -1,20 +1,23 @@
 const Order = require("../models/Order");
 
-module.exports.orderRoute = async (req,res) => {
-    let {email,data} = req.body;
-    await data.splice(0,0,{Order_date : req.body.order_date})
+const orderRoute = async (req,res) => {
+    let {email,order_data : data} = req.body;
+    // console.log(req.body);
+    await data.splice(0,0,{Order_date : req.body.order_date}) // splice(start, delCount, items to add...)
 
     // if it's the first order of a user
     let isUser = await Order.findOne({email})
     console.log("isUser : ",isUser);
     if(!isUser){
         let newOrder = await Order.create({email, order_data : data})
-        console.log("new order : ",newOrder);
+        // console.log("new order : ",newOrder);
         res.status(200).json({success : true})
     }
     else{ // user has ordered in past
         let updatedOrder = await Order.findOneAndUpdate({email},{$push : {order_data : data}})
-        console.log("updatedOrder : ",updatedOrder);
+        // console.log("updatedOrder : ",updatedOrder);
         res.status(200).json({success : true})
     }
 }
+
+module.exports = orderRoute
