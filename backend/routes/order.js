f63be690fa58859a63a518ee468db590b0a395e6
Ashu Negi <anegi6723@gmail.com>
2024-03-22 11:14:33 +0530

    modified route of order

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const orderRoute = require('../controller/order');
 
-router.post('/order', asyncHandler(orderRoute))
+router.post('/', asyncHandler(orderRoute))
 
 module.exports = router;
