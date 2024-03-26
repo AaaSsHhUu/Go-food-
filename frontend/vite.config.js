bcc7fcf45d04b2c4dc2162d20e5fe4e9cbdcaacb
Ashu Negi <anegi6723@gmail.com>
2024-03-26 06:07:50 +0530

    created separate folder for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-      "/api" : "http://localhost:5000"
+      "/api" : "https://go-food-cvm6.onrender.com"
     }
   },
   plugins: [react()],
