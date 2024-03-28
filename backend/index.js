42d411b9f2265fbf43115c9443d66ccb430b58ef
Ashu Negi <anegi6723@gmail.com>
2024-03-28 20:46:19 +0530

    build successful

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const app = express();
 const connectDB = require("./db");
 const port = 5000;
 const dotenv = require("dotenv").config();
+const path = require("path")
 
 // Connecting to Database
 connectDB()
 
 // app.use(cors);
 
+app.use(express.static(path.join(__dirname,"/dist")));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
